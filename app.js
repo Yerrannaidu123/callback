59fbed1bbe6c6fbf6eb32bce2692729d10840843
Yerrannaidu123 <chinnababubadagala@gmail.com>
2023-08-21 23:22:42 +0530

    commiting callbackpractice

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// function callbacktest(){
+//     console.log("test is passed");
+// }
+// function test1(){
+//     console.log("test is again passed");
+// }
+
+
+// function javascript(){
+    
+    for (var i=0;i<=20**2;i++){
+        if (i%2==0 ){
+            console.log("even")
+        }else{
+            console.log(i)
+        }
+    }
+// }
+
+
+// function girls(a,c,test,test2,small){
+//     console.log(a+c);
+//     test()
+//     test2()
+//     small()
+// }
+// let f=4;
+// let s=3;
+
+// girls(f,s,callbacktest,test1,javascript)
