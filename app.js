a747cabc3823b697ee9ef2dbc7eca1aa14bb9232
Yerrannaidu123 <chinnababubadagala@gmail.com>
2023-08-22 00:32:25 +0530

    commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
-// function callbacktest(){
-//     console.log("test is passed");
-// }
-// function test1(){
-//     console.log("test is again passed");
-// }
+function callbacktest(){
+    console.log("test is passed");
+}
+function test1(){
+    console.log("test is again passed");
+}
 
 
-// function javascript(){
+function javascript(){
     
     for (var i=0;i<=20**2;i++){
         if (i%2==0 ){
@@ -16,16 +16,16 @@
             console.log(i)
         }
     }
-// }
+}
 
 
-// function girls(a,c,test,test2,small){
-//     console.log(a+c);
-//     test()
-//     test2()
-//     small()
-// }
-// let f=4;
-// let s=3;
+function girls(a,c,test,test2,small){
+    console.log(a+c);
+    test()
+    test2()
+    small()
+}
+let f=4;
+let s=3;
 
-// girls(f,s,callbacktest,test1,javascript)
+girls(f,s,callbacktest,test1,javascript)
