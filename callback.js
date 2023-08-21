a746aa670e82d3379793df7b970cb5a42abb184f
Yerrannaidu123 <chinnababubadagala@gmail.com>
2023-08-21 23:56:32 +0530

    chinna

---
+++ callback.js
@@ -0,0 +1,14 @@
+function dudu(){
+    console.log("i love you")
+}
+function ishq(){
+    console.log("chinna proposed tanuja...but she rejected intially. after, she fell in love with chinna and she accepted him. ")
+}
+function bubu(c,t,love,thanuu){
+    console.log(c +" "+"loves"+" "+ t);
+    love()
+    thanuu()
+}
+let c = "chinna";
+let t = "tanu";
+bubu(c,t,dudu,ishq)
